f105ab90f00e815deaa166afd488134b17b83b49
HeikoDudzus <heiko.dudzus@gmail.com>
2016-07-05 13:09:16 +0200

    Fehlerbehandlung mit Try-Catch und Startklasse
    Fehlerbehandlung im GameServer mit Try-Catch, falls Gegenspieler nicht
    mehr existiert, Startklasse hinzugefügt

---
+++ Start.java
@@ -0,0 +1,14 @@
+
+/**
+ * Klasse zum Starten als Anwendung
+ * 
+ * @author Heiko Dudzus (Die Hinterbänkler) 
+ * @version 2016-07-05
+ */
+public class Start
+{
+    public static void main(String[] args)
+    {
+        new GUI();
+    }
+}
